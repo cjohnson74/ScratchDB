b0ab50dcd790186de864bd5b7693600201a2d17f
Carson Johnson <cjohnson74.tech@gmail.com>
2026-06-03 20:43:10 -0700

    Tested Open function

--- api.go
+++ api.go
@@ -2,13 +2,13 @@ package scratchdb
 
 import (
 	"os"
+	"path/filepath"
 	uuid "github.com/google/uuid"
 )
 
 type ScratchDB struct {
-	// TODO 1: Fill in ScratchDB struct. What does ScratchDB need to hold internally?
 	dir				string
-	keyDir			map[uuid.UUID][]byte
+	keyDir			map[string][]byte
 	activeFile		string
 	options			Options
 }
@@ -24,17 +24,16 @@ func createEmptyFile(name string) error {
 	return err
 }
 
-// TODO 2: implement Open function
 func Open(directoryName string, options Options) (*ScratchDB, error) {
 	var err error
 
 	err = os.Mkdir(directoryName, 0755)
 
-	if err != nil {
+	if err != nil && !os.IsExist(err) {
 		return nil, err
 	}
 
-	dataFile := uuid.NewString()
+	dataFile := filepath.Join(directoryName, uuid.NewString())
 
 	err = createEmptyFile(dataFile)
 
@@ -44,7 +43,7 @@ func Open(directoryName string, options Options) (*ScratchDB, error) {
 
 	newDB := ScratchDB{
 		dir:				directoryName,
-		keyDir:				make(map[uuid.UUID][]byte),
+		keyDir:				make(map[string][]byte),
 		activeFile:			dataFile,
 		options:			options,
 	}
@@ -52,8 +51,8 @@ func Open(directoryName string, options Options) (*ScratchDB, error) {
 	return &newDB, err
 }
 
-func (db *ScratchDB) Get(key []byte) ([]byte, error)
 func (db *ScratchDB) Put(key []byte, value []byte) error
+func (db *ScratchDB) Get(key []byte) ([]byte, error) 
 func (db *ScratchDB) Delete(key []byte) error
 func (db *ScratchDB) ListKeys() ([][]byte, error)
 func (db *ScratchDB) Fold(fn func(key []byte, value []byte, acc any) any, acc any) (any, error)
--- api_test.go
+++ api_test.go
@@ -1,5 +1,38 @@
 package scratchdb
 
-import "testing"
+import (
+	"testing"
+	"os"
+)
 
 // TODO 3: Add unit table-driven tests of Open function
+func TestOpenDB(t *testing.T) {
+	defer os.RemoveAll("test")
+
+	testDB, err := Open("test", Options{true, false})
+	if err != nil {
+		t.Fatalf("failed to open: %v", err)
+	}
+
+	if _, err := os.Stat("test"); os.IsNotExist(err) {
+		t.Fatalf("expected directory 'test' to exist")
+	}
+
+	if _, err := os.Stat(testDB.activeFile); os.IsNotExist(err) {
+		t.Fatalf("expected active file %s to exist", testDB.activeFile)
+	}
+
+	if testDB.options.ReadWrite == false {
+		t.Fatalf("option.ReadWrite == false; should be true")
+	}
+
+	if testDB.options.SyncOnPut == true {
+		t.Fatalf("options.SyncOnPut == true; should be false")
+	}
+}
+
+func TestOpenCloseOpen(t *testing.T) {}
+
+func TestOpenPut(t *testing.T) {}
+
+func TestNotOpenPut(t *testing.T) {}
